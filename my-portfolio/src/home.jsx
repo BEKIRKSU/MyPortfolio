07d60728e1cbb729cb22b2ba76b5ae8b1494711d
BEKIRKSU <bekirkscu@gmail.com>
2023-07-31 03:14:53 +0100

    Beginning

---
+++ my-portfolio/src/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="App">
+    <header className="App-header">
+      <h1>My Portfolio</h1>
+      <button>Make my digital portfolio</button>
+    </header>
+  </div>
+  );
+};
+
+export default HomePage;
