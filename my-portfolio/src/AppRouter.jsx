b1e191e62d333f88b83fd7d495adc73552955e09
BEKIRKSU <bekirkscu@gmail.com>
2023-08-04 23:16:38 +0100

    App Router

--- my-portfolio/src/AppRouter.jsx
+++ my-portfolio/src/AppRouter.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import Profile from './Profile';
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Home from './Home';
+// import Profile from './Profile';
 
-const AppRouter = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Profile" element={<Profile />} />
-      </Routes>
-    </Router>
-  );
-};
+// const AppRouter = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/Profile" element={<Profile />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
 
-export default AppRouter;
+// export default AppRouter;
