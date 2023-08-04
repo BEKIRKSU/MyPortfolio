164a637a65674a8fa3656b07e6fdcf3759338a19
BEKIRKSU <bekirkscu@gmail.com>
2023-08-04 23:04:33 +0100

    -

--- my-portfolio/src/home.jsx
+++ my-portfolio/src/home.jsx
@@ -4,14 +4,15 @@ import { Link } from 'react-router-dom';
 const HomePage = () => {
   return (
     <div className="App">
-      {/* Profile Button */}
       <Link to="/profile">
         <button className="profile-button">Profile</button>
       </Link>
 
       <header className="App-header">
         <h1>My Portfolio</h1>
+        <Link to="/Sign-up">
         <button class="make-my-portfolio-button">Make my digital portfolio</button>
+        </Link>
       </header>
     </div>
   );
