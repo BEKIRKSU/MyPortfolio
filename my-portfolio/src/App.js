2fd67bd28611a3266a872083f9991d46c5f7f216
BEKIRKSU <bekirkscu@gmail.com>
2023-08-04 22:39:53 +0100

    App.js updated

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar';
 import Home from './Home';
 import Profile from './Profile';
 
@@ -9,6 +10,7 @@ function App() {
     <>
     <Router>
       <Routes>
+        <Route path="/navbar" element={<Navbar />} />
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
@@ -18,4 +20,10 @@ function App() {
 }
 
 export default App;
-       
+
+
+
+
+
+
+
