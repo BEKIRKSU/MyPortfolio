8d395e337ab4887f0797ba8c06c2ea59fbb215b8
BEKIRKSU <bekirkscu@gmail.com>
2023-08-04 22:40:55 +0100

    -

--- my-portfolio/src/Navbar.jsx
+++ my-portfolio/src/Navbar.jsx
@@ -4,18 +4,6 @@ import { Link } from 'react-router-dom';
 const Navbar = ({ onBackToHome }) => {
   return (
     <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        {/* Add more menu items as needed */}
-      </ul>
       <button className="back-to-home" onClick={onBackToHome}>
         Back to Home
       </button>
