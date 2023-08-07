ab52ac590c47dbc92096f6859000b900c276080f
BEKIRKSU <bekirkscu@gmail.com>
2023-08-07 16:41:02 +0100

    -

--- my-portfolio/src/Navbar.jsx
+++ my-portfolio/src/Navbar.jsx
@@ -7,10 +7,10 @@ const Navbar = ({ onBackToHome }) => {
       <div className="name-area">
         <h2 className="name-surname">NAME SURNAME</h2>
         <h1 className="watermark">V/U</h1>
+        </div>
       <button className="sign-out" onClick={onBackToHome}>
         Sign out
       </button>
-      </div>
     </nav>
   );
 };
