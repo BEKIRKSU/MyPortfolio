1e0239b4b9b0c0a80cf84349c3c5c3f8708edb3d
BEKIRKSU <bekirkscu@gmail.com>
2023-08-07 16:06:13 +0100

    Button name changed to 'Sign out'

--- my-portfolio/src/Navbar.jsx
+++ my-portfolio/src/Navbar.jsx
@@ -4,8 +4,8 @@ import './navbar.css';
 const Navbar = ({ onBackToHome }) => {
   return (
     <nav>
-      <button className="back-to-home" onClick={onBackToHome}>
-        Back to Home
+      <button className="sign-out" onClick={onBackToHome}>
+        Sign out
       </button>
     </nav>
   );
