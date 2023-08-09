2b6f831cc74c29413dd09c1167a32a4803e35c32
BEKIRKSU <bekirkscu@gmail.com>
2023-08-09 19:15:20 +0100

    Update ProfilePage.jsx

--- my-portfolio/src/ProfilePage.jsx
+++ my-portfolio/src/ProfilePage.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
 import Navbar from './Navbar';
-import './profile-page.css';
+import './ProfilePage.css';
 import CountryList from './CountryList';
 import ProfileForm from './ProfileForm';
 import MediaUpload from './MediaUpload';
