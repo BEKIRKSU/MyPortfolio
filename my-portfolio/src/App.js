229b73443528a2ddaa4e149e0601afc7134b170b
BEKIRKSU <bekirkscu@gmail.com>
2023-08-14 13:33:12 +0100

    Update App.js

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -9,18 +9,49 @@ import SignUp from './Sign-up';
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/navbar" element={<Navbar />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/sign-up" element={<SignUp />} />
-      </Routes>
-    </Router>
+    <div className="app-container">
+       <header>
+        {/* Header content */}
+        </header>
+        <main>
+          {/* Main content */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/navbar" element={<Navbar />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/sign-up" element={<SignUp />} />
+          </Routes>
+        </main>
+        <footer>
+          {/*  Footer content */}
+    </Router> 
   );
 }
 
 export default App;
 
+
+
+        
+      
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/navbar" element={<Navbar />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/sign-up" element={<SignUp />} />
+          </Routes>
+        </main>
+        
+        <footer>
+          {/* Your footer content */}
+          <p>Footer</p>
+        </footer>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
 
 
   
