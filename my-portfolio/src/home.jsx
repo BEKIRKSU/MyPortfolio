9275381facafc9505693a390f57f9a0a55cbd286
BEKIRKSU <bekirkscu@gmail.com>
2023-10-13 20:03:17 +0100

    Update home.jsx

--- my-portfolio/src/home.jsx
+++ my-portfolio/src/home.jsx
@@ -6,7 +6,7 @@ const HomePage = () => {
   return (
     <div className="App">
       <Link to="/profile">
-        <button className="profile-button">Proile</button>
+        <button className="profile-button">Profile</button>
       </Link>
 
       <header className="App-header">
