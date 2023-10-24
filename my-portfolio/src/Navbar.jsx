f1e8734eb47395d6228827a89c3cb176df781967
BEKIRKSU <bekirkscu@gmail.com>
2023-10-24 16:34:16 +0100

    Name changed

--- my-portfolio/src/Navbar.jsx
+++ my-portfolio/src/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({ onBackToHome }) => {
     <nav>
       <div className="name-area">
       <img src="https://img.freepik.com/premium-vector/bk-logo_590037-244.jpg?w=2000" alt="Logo" />
-        <h2 className="name-surname">NAME SURNAME</h2>
+        <h2 className="name-surname">FULL NAME</h2>
       </div>
       <button className="sign-out" onClick={onBackToHome}>
         Sign out
